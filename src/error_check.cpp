adb2fd0fb2554ef19a53aaeb98022e4127901693
schmips <padmanaba@hotmail.co.uk>
2017-11-23 17:54:28 +0000

    Error checking complete
    Memory addresses updated to reflect memory changes

--- src/error_check.cpp
+++ src/error_check.cpp
@@ -2,21 +2,21 @@
 
 bool RW_error(int access_addr)
 {
-    if (access_addr > 0x4000000) return true;
+    if (access_addr < 0x8000000 || access_addr >= 0x9000000) return true;
     return false;
 }
 
 
 bool mem_range_error(int access_addr)
 {
-    if (access_addr > 0x30000008) return true;
+    if (access_addr >= 0x40000000) return true;
     return false;
 }
 
 
 bool invalid_instruction(int access_addr)
 {
-    if (access_addr > 0x1FFFFFFF || access_addr < 0x4) return true;
+    if (access_addr < 0x9000000 || access_addr >= 0x4400000) return true;
     return false;
 }
 
