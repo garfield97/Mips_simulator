70448ef5f1829257b44740714912d0ab126e0598
schmips <padmanaba@hotmail.co.uk>
2017-11-23 17:47:23 +0000

    Adding functionality to error checking
    Added arithemtic error checks: divide by 0 and overflow

--- src/error_check.cpp
+++ src/error_check.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 
 bool RW_error(int access_addr)
 {
@@ -16,5 +17,18 @@ bool mem_range_error(int access_addr)
 bool invalid_instruction(int access_addr)
 {
     if (access_addr > 0x1FFFFFFF || access_addr < 0x4) return true;
+    return false;
+}
+
+bool addition_exception(unsigned short op1, unsigned int short op2)
+{
+    if (op1 > INT_MAX - op2) return true;
+    return false;
+}
+
+
+bool division_error(unsigned short op1, unsigned int short op2)
+{
+    if (op2 == 0) return true;
     return false;
 }
