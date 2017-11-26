cc45bd782fdedfaee80f69532c334275a192f502
schmips <padmanaba@hotmail.co.uk>
2017-11-26 17:08:15 +0000

    changes

--- src/testbench.cpp
+++ src/testbench.cpp
@@ -1,10 +1,16 @@
 #include "father.cpp"
 #include "instructions.hpp"
+#include <string>
 
 using namespace std;
 
 
 int main()
 {
-
+    string instruction, type;
+    int rs, rt, rd, IMM, shift;
+    while(instruction != "exit")
+    {
+        
+    }
 }
