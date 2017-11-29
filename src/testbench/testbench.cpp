d75f0d77bd026929c386d19e5dac7e0950b35ae3
schmips <padmanaba@hotmail.co.uk>
2017-11-29 13:18:26 +0000

    changes

--- src/testbench/testbench.cpp
+++ src/testbench/testbench.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <fstream>
+
 
 using namespace std;
 
-int main(){
-  cout << "aaaaaaaahahhh!!" << endl;
+
+int main()
+{
+  fstream csv_out;
+  ofstream tst_in;
+  int test_size;
+  string in_base = "tst";
+  string in_name;
+
+  cout<<"Enter the number of test files"<<endl;
+  cin>>test_size;
+
+  csv_out.open("results.csv");
+
+  csv_out<<"TestId , Instruction , Status , Author [, Message]"<<endl;
+
+  for (int i = 0; i < test_size; i++)
+  {
+    in_name = in_base + (char)i + ".bin";
+
+    tst_in.open(in_name.c_str());
+
+    // Do testing and get output
+
+    write_csv();
+  }
+
+}
+
+
+void write_csv()
+{
+
 }
