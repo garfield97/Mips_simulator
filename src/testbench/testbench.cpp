e9ab89636bd3fb484722dad00ff016641cee0c39
schmips <padmanaba@hotmail.co.uk>
2017-11-29 13:31:12 +0000

    testbench framework

--- src/testbench/testbench.cpp
+++ src/testbench/testbench.cpp
@@ -5,6 +5,9 @@
 using namespace std;
 
 
+void write_csv(fstream &outfile, int id, string instruction, bool status, string author, string message);
+
+
 int main()
 {
   fstream csv_out;
@@ -12,11 +15,20 @@ int main()
   int test_size;
   string in_base = "tst";
   string in_name;
+  string author_name;
+  bool pass = false;
+  string inst;
+  string message;
 
-  cout<<"Enter the number of test files"<<endl;
+  cout<<"Enter the number of test files:"<<endl;
   cin>>test_size;
 
+  cout<<"Enter author name:"<<endl;
+  cin>>author_name;
+
   csv_out.open("results.csv");
+
+  if (!csv_out.is_open()) cout<<"Cannot open output file<<endl"<<endl; exit(EXIT_FAILURE);
 
   csv_out<<"TestId , Instruction , Status , Author [, Message]"<<endl;
 
@@ -26,15 +38,26 @@ int main()
 
     tst_in.open(in_name.c_str());
 
-    // Do testing and get output
+    // Do testing and get result
 
-    write_csv();
+    write_csv(csv_out, i, inst, pass, author_name, message); //Print result in csv file and move on to next test
+
+    tst_in.close();
   }
 
+  csv_out.close();
+
+  return 0;
 }
 
 
-void write_csv()
+void write_csv(fstream &outfile, int id, string instruction, bool status, string author, string message)
 {
+  if (status)
+  {
+    outfile<<id<<", "<<instruction<<", "<<"Pass"<<", "<<author<<", "<<message<<", "<<endl; 
+    return;
+  }
 
+  outfile<<id<<", "<<instruction<<", "<<"Fail"<<", "<<author<<", "<<message<<", "<<endl;
 }
