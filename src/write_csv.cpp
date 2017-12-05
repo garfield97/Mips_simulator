9f365cbd0bb02efa4cc54383f28d13dbcb0180d6
oscario97 <oscario97@gmail.com>
2017-12-05 18:04:55 +0000

    Cases for error. changes

--- src/write_csv.cpp
+++ src/write_csv.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-void write_csv(fstream &outfile, string id, string instruction, bool status, string author);
+void write_csv(fstream &outfile, string id, string instruction, bool status, string author, string argument);
 
 
 int main(int argc, char **argv)
@@ -17,8 +17,19 @@ int main(int argc, char **argv)
     string type = argv[6];
     string tmp;
     string argument;
-
-
+    if(type =="-1") argument = "No errors";
+    
+    else if(type == "-10") argument = "Arithmetic errror";
+    
+    else if(type == "-11") argument =  "Memory Exception";
+    
+    else if(type == "-12") argument =  "Invalid Instruction";
+    
+    else if(type == "-20") argument =  "Internal Error";
+    
+    else argument = "Unkown error";
+      
+        
     bool passed = false;
 
     if (pass == "pass") passed = true;
@@ -37,7 +48,7 @@ int main(int argc, char **argv)
     getchar();
     cout<<"schmei"<<endl;
 
-    csv_out<<"TestId , Instruction , Status , Author"<<endl;
+    csv_out<<"TestId , Instruction , Status , Author, Message"<<endl;
 
     write_csv(csv_out, ref, test_name, passed, name);
 
@@ -47,13 +58,13 @@ int main(int argc, char **argv)
 }
 
 
-void write_csv(fstream &outfile, string id, string instruction, bool status, string author)
+void write_csv(fstream &outfile, string id, string instruction, bool status, string author, string argument)
 {
   if (status)
   {
-    outfile<<id<<", "<<instruction<<", "<<"Pass"<<", "<<author<<", "<<endl; 
+    outfile<<id<<", "<<instruction<<", "<<"Pass"<<", "<<author<<", "<<argument<<", "; 
     return;
   }
 
-  outfile<<id<<", "<<instruction<<", "<<"Fail"<<", "<<author<<", "<<endl;
+  outfile<<id<<", "<<instruction<<", "<<"Fail"<<", "<<author<<", "<<argument<<", "; 
 }
