ad6f27997f84087fd395611062a4b7eb401630d7
schmips <padmanaba@hotmail.co.uk>
2017-12-05 18:15:29 +0000

    changes

--- testbench.sh
+++ testbench.sh
@@ -18,16 +18,32 @@ do
 	command="$1 $name"
 	result=$($command)
 
-	export output_filename passed name username counter
+	export output_filename passed name username counter type
 
 	case $result in
 	-1)
-		arguments="$output_filename $passed $name $username $counter"
-		echo $arguments
+		type=-1
+		arguments="$output_filename $passed $name $username $counter $type"
+		;;
+	-10)
+		type=-10
+		arguments="$output_filename $failed $name $username $counter $type"
+		;;
+	-11)
+		type=-11
+		arguments="$output_filename $failed $name $username $counter $type"
+		;;
+	-12)
+		type=-12
+		arguments="$output_filename $failed $name $username $counter $type"
+		;;
+	-20)
+		type=-20
+		arguments="$output_filename $failed $name $username $counter $type"
 		;;
 	*)
-		arguments="$output_filename $failed $name $username $counter"
-		echo $arguments
+		type=0
+		arguments="$output_filename $failed $name $username $counter $type"
 		;;
 	esac
 
