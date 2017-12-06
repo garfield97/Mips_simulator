4b6935fd3b0bd2258702159d6a57a047f6cd3502
schmips <padmanaba@hotmail.co.uk>
2017-12-06 15:15:37 +0000

    changes

--- src/write_csv.cpp
+++ src/write_csv.cpp
@@ -17,15 +17,10 @@ int main(int argc, char **argv)
     string argument;
 
     if(type =="-1") argument = "No errors";
-    
     else if(type == "-10") argument = "Arithmetic errror";
-    
     else if(type == "-11") argument =  "Memory exception";
-    
     else if(type == "-12") argument =  "Invalid instruction";
-    
     else if(type == "-20") argument =  "Internal error";
-    
     else argument = "Unkown error";
         
     bool passed = false;
